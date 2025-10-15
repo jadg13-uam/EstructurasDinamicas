6511ec087cc57acc6521fc9d3c383e091a05fffa
jadg <jose.duran@uamv.edu.ni>
2025-10-15 10:24:38 -0600

    formulario diseño

--- EstructuraDinamicaG6/Form1.Designer.cs
+++ EstructuraDinamicaG6/Form1.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblEdades = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edad:";
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(120, 70);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 32);
+            this.tbEdad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(240, 51);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(226, 59);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lblEdades
+            // 
+            this.lblEdades.AutoSize = true;
+            this.lblEdades.Location = new System.Drawing.Point(24, 156);
+            this.lblEdades.Name = "lblEdades";
+            this.lblEdades.Size = new System.Drawing.Size(83, 26);
+            this.lblEdades.TabIndex = 3;
+            this.lblEdades.Text = "edades";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 450);
+            this.ClientSize = new System.Drawing.Size(496, 731);
+            this.Controls.Add(this.lblEdades);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblEdades;
     }
 }
 
